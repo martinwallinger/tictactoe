f8cf6da5da65e9b999eed4c64efe0b9769835167
Martin <wallim02.mw@gmail.com>
2022-06-20 18:24:46 +0200

    corrected tests (mocker works)

--- test/test_model.py
+++ test/test_model.py
@@ -1,95 +1,76 @@
+import tictactoe.view
 from tictactoe import model
-from unittest.mock import Mock
+from tictactoe import view
+import pytest_mock
 
 
-def test_is_game_over__not_over():
-    board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    answer = model.is_game_over(board)
-    assert answer == 0
+class Test:
+    def test_is_game_over__not_over(self):
+        board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        answer = model.is_game_over(board)
+        assert answer == 0
 
+    def test_is_game_over__draw(self):
+        board = [["X", "O", "X"], ["X ", "O", "O"], ["O", "X", "X"]]
+        answer = model.is_game_over(board)
+        assert answer == 'draw'
 
-def test_is_game_over__draw():
-    board = [["X", "O", "X"], ["X ", "O", "O"], ["O", "X", "X"]]
-    answer = model.is_game_over(board)
-    assert answer == 'draw'
+    def test_is_game_over__o_won(self):
+        board = [["O", "O", "O"], [" ", " ", " "], [" ", " ", " "]]
+        answer = model.is_game_over(board)
+        assert answer == 'O'
 
+    def test_is_game_over__x_won(self):
+        board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
+        answer = model.is_game_over(board)
+        assert answer == 'X'
 
-def test_is_game_over__o_won():
-    board = [["O", "O", "O"], [" ", " ", " "], [" ", " ", " "]]
-    answer = model.is_game_over(board)
-    assert answer == 'O'
+    def test_new_board(self):
+        board = model.new_board()
 
+        assert isinstance(board, list)
 
-def test_is_game_over__x_won():
-    board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
-    answer = model.is_game_over(board)
-    assert answer == 'X'
+        for row in board:
+            for element in row:
+                assert element == " "
 
+    def test_save_move__empty_board(self):
+        move = (1, 1)
+        player = 'X'
+        board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        board = model.save_move(move, player, board)
+        player = 'O'
+        move = (0, 0)
+        board = model.save_move(move, player, board)
 
-def test_new_board():
-    board = model.new_board()
+        assert isinstance(board, list)
+        assert board == [["O", " ", " "], [" ", "X", " "], [" ", " ", " "]]
 
-    assert isinstance(board, list)
+    def test_save_move__save(self):
+        move = "save"
+        player = 'X'
+        board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        assert model.save_move(move, player, board) is False
 
-    for row in board:
-        for element in row:
-            assert element == " "
+    def test_save_move__move_taken(self, mocker):
+        mocker.patch('tictactoe.view.get_move')
+        tictactoe.view.get_move.return_value = (0, 0)
+        move = (1, 1)
+        player = 'X'
+        board = [[" ", " ", " "], [" ", "O", " "], [" ", " ", " "]]
+        board = model.save_move(move, player, board)
 
+        tictactoe.view.get_move.assert_called_once()
+        assert board == [["X", " ", " "], [" ", "O", " "], [" ", " ", " "]]
 
-def test_save_move__empty_board():
-    move = (1, 1)
-    player = 'X'
-    board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    board = model.save_move(move, player, board)
-    player = 'O'
-    move = (0, 0)
-    board = model.save_move(move, player, board)
+    def test_save_move__move_save(self, mocker):
+        mocker.patch('tictactoe.view.get_move')
+        tictactoe.view.get_move.return_value = "save"
+        move = (1, 1)
+        player = 'O'
+        board = [[" ", " ", " "], [" ", "O", " "], [" ", " ", " "]]
+        board = model.save_move(move, player, board)
 
-    assert isinstance(board, list)
-    assert board == [["O", " ", " "], [" ", "X", " "], [" ", " ", " "]]
-
-
-def test_save_move__save():
-    move = "save"
-    player = 'X'
-    board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    assert model.save_move(move, player, board) is False
-
-
-def test_save_move__move_taken():
-    view = Mock()
-    view.get_move.return_value = (0, 0)
-
-    move = (1, 1)
-    player = 'X'
-    board = [[" ", " ", " "], [" ", "O", " "], [" ", " ", " "]]
-    board = model.save_move(move, player, board)
-
-    assert view.assert_called()
-    assert board == [["X", " ", " "], [" ", "O", " "], [" ", " ", " "]]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        tictactoe.view.get_move.assert_called_once()
+        assert board is False
 
